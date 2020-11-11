9c7e4c3289f19317a7a39d5897a6a961d9380d67
Ashish <ashish.saxena25@gmail.com>
2020-11-11 05:20:24 -0500

    updated project with styles.css and web_script.js

---
+++ web_script.js
@@ -0,0 +1,13 @@
+var students = ["John" , "Jacob" , "Jingle" , "Heimer", "Smith"];
+
+for(x=0; x < students.length; x++){
+    console.log(students[x]);
+}
+
+function area(len, wid) {
+    return(len*wid);
+}
+
+var x = 10;
+var y = 5;
+console.log("Area is: " + area(x,y))
